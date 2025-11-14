27897dd61653a74690dd473865fb273b3852cefc
MLSALEX <stiopovna@mail.ru>
2025-11-14 12:31:26 +0200

    feat(auth): limit phone input to country format and stop input after max digits

--- app/src/main/java/com/alexmls/lazypizza/authorization/presentation/helpers/PhoneInputFormat.kt
+++ app/src/main/java/com/alexmls/lazypizza/authorization/presentation/helpers/PhoneInputFormat.kt
@@ -1,10 +1,17 @@
 package com.alexmls.lazypizza.authorization.presentation.helpers
 
+private const val MAX_PHONE_DIGITS = 11
+
 internal fun String.toPhoneInput(): String = buildString {
+    var digitsCount = 0
+
     this@toPhoneInput.forEachIndexed { index, ch ->
         when {
             ch == '+' && index == 0 -> append(ch)
-            ch.isDigit() -> append(ch)
+            ch.isDigit() && digitsCount < MAX_PHONE_DIGITS-> {
+                append(ch)
+                digitsCount++
+            }
         }
     }
 }
